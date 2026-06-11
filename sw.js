35079e015edabb002d4929a13b343581f813fc7c
tahahumam233-blip <tahahumam233@gmail.com>
2026-06-11 16:19:18 +0300

    Premium cyber-arena visuals: layered animated backdrop, energy court, comet ball, racket paddles, orb power-ups, countdown, win screen with stats, quality setting, dt-based FX, haptics

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = 'campong-v2';
+const C = 'campong-v3';
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(C).then(c => c.addAll(['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'])));
   self.skipWaiting();
